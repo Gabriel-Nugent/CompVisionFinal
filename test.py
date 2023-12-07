5d11c8b53675d3d7b495381bc551174a9258c5ae
Gabriel-Nugent <gabriel.nugent209@gmail.com>
2023-12-06 18:36:13 -0600

    pulled

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-import numpy as np
-import cv2
 import os
 import config
 import sys
+import numpy as np
+import cv2
+
+from boosting import boosted_predict
 from skin_detection import detect_skin
+from draw_rectangle import draw_rectangle
 
 # Get the absolute path of the script's directory
 current_directory = os.path.abspath(os.path.dirname(__file__))
@@ -27,6 +30,8 @@ def test():
 
     faces = os.path.join(current_directory, training_directory, 'test_face_photos')
     nonfaces = os.path.join(current_directory, training_directory, 'test_nonfaces')
+    boosted_model = np.load('results/boosted_model.npy')
+    weak_classifiers = np.load('results/weak_classifiers.npy')
 
     false_positives = 0
     false_negatives = 0
@@ -38,16 +43,45 @@ def test():
             # Load the image
             image = cv2.imread(filename)
             image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image_grey = cv2.cvtColor(image, cv2.COLOR_BAYER_BG2GRAY)
+
             skin_mask = test_skin(image_rgb)
+            skin_windows = []
+            window_indexes = []
 
-            # USE MODEL FOR FACE DETECTION
+            # Create 100x100 windows around each skin pixel
+            window_size = 100
+            for pixel in skin_mask:
+                y, x = pixel
+                if y - window_size//2 >= 0 and y + window_size//2 < image_grey.shape[0] and x - window_size//2 >= 0 and x + window_size//2 < image_grey.shape[1]:
+                    skin_window = image_grey[y - window_size//2 : y + window_size//2, x - window_size//2 : x + window_size//2]
+                    skin_windows.append(skin_window)
+            
+            
+            results = boosted_predict(skin_windows, boosted_model, weak_classifiers, 1)
+            positive_results_indices = [skin_windows[i] for i in range(len(results)) if results[i] > 0]
+            positive_results = [skin_windows[i] for i in positive_results_indices]
+
+            results = boosted_predict(positive_results, boosted_model, weak_classifiers, 5)
+            positive_results_indices = [skin_windows[i] for i in range(len(results)) if results[i] > 0]
+            positive_results = [skin_windows[i] for i in positive_results_indices]
+
+            results = boosted_predict(positive_results, boosted_model, weak_classifiers, 25)
+            positive_results_indices = [skin_windows[i] for i in range(len(results)) if results[i] > 0]
+            positive_results = [skin_windows[i] for i in positive_results_indices]
+
+            # for face in positive_results:
+            #     top = positive_results[face]
+            #     image_result = draw_rectangle(image_rgb)
+
     
-    for filename in os.listdir(nonfaces):
-        # Load the image
-        image = cv2.imread(filename)
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        skin_mask = test_skin(image_rgb)
+    # for filename in os.listdir(nonfaces):
+    #     # Load the image
+    #     image = cv2.imread(filename)
+    #     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    #     image_grey = cv2.cvtColor(image, cv2.COLOR_BAYER_BG2GRAY)
+    #     skin_mask = test_skin(image_rgb)
 
-        #USE MODEL FOR FACE DETECTION
+    #     #USE MODEL FOR FACE DETECTION
 
     return false_positives, false_negatives
